709114eef4f59cce2a1f8a501eddc16cc74b63d5
Dawood Akbar <dawood_akbar@ml-bench.com>
2026-05-05 15:39:29 +0500

    Initial Commit

---
+++ FireLime/FireLimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireLimeApp.swift
+//  FireLime
+//
+//  Created by Apple on 05/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct FireLimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
